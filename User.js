476e92304e4e345773a74c94ef6136b954f41941
alpha <123271697+alphaaa2-hash@users.noreply.github.com>
2025-12-20 22:51:15 +0100

    Create User model with schema definition

---
+++ User.js
@@ -0,0 +1,16 @@
+// models/User.js
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  beats: [
+    {
+      filename: String,
+      path: String
+    }
+  ]
+});
+
+module.exports = mongoose.model("User", userSchema);
